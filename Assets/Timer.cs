27af86ee8c351d80f7f465f038691e369c93a799
awesomeedl <82195917+awesomeedl@users.noreply.github.com>
2022-05-22 22:31:36 -0700

    respawn system

---
+++ Assets/Timer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    GameObject player;
+    public List<Transform> spawnPoints;
+    public float levelTime = 20f;
+    float currentTime;
+
+    public TMP_Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>().gameObject;
+        currentTime = levelTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDown();
+    }
+
+    void CountDown() {
+        if(currentTime > 0) {
+            currentTime -= Time.deltaTime;
+            timerText.text = "Remaining Time: " + currentTime.ToString("N");
+        }
+        else
+        {
+            Respawn();
+            currentTime = levelTime;
+        }
+    }
+
+    void Respawn() {
+        player.transform.position = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
+    }
+}
